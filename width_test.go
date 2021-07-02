8192affa0933543e8954b093ef364ddb85a1fb4c
Aleksandr Novikov <mem-memov@yandex.ru>
2021-07-02 22:38:57 +0300

    test added

--- width_test.go
+++ width_test.go
@@ -10,10 +10,12 @@ func TestCalculateWidth(t *testing.T) {
 		expected uint
 	}{
 		{"one line, one byte width, min value", 0, 1, 1},
-		{"one line, one byte width, man value", 254, 1, 1},
+		{"one line, one byte width, max value", 254, 1, 1},
 		{"one line, two byte width, min value", 255, 1, 2},
 		{"one line, two byte width, max value", 65534, 1, 2},
 		{"one line, three byte width, min value", 65535, 1, 3},
+		{"two lines, one byte width, min value", 0, 2, 1},
+		{"two lines, one byte width, max value", 253, 2, 1},
 	}
 
 	for _, d := range data {
